a5c745b52ddc5a63782432f7af459e5818de67a1
DESKTOP-84QENAL\Alejandro Arochi <arochi.avalos.369@gmail.com>
2025-08-17 01:42:33 -0600

    AAA_17082025 .- Se creo la estructura clean architecture.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,7 @@ import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+// import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,5 +19,13 @@ export const appConfig: ApplicationConfig = {
     provideStore(),
     provideEffects(),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+
+    // Configuracion para el depliegue en servidores por Ej. Netlify o Amplify
+    // para que no se pierda la aplicacion al hacer refresh en el Navegador.
+    // HashStrategy
+    // {
+    //   provide: LocationStrategy,
+    //   useClass: HashLocationStrategy
+    // }
 ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './UI/auth/page/auth.component/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
